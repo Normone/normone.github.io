ef3af374ea0e0ec93a8a865849c4c673a56e4749
Normone <vashhekin0d0@gmail.com>
2022-02-14 00:49:15 +0700

    a

--- js/d-d-list.js
+++ js/d-d-list.js
@@ -1,75 +1,90 @@
 document.addEventListener("DOMContentLoaded", function(){
 
-    // $('.d-dlist').each(function (idx, el) {
-    //     $(el).click( function (e) {
+    $('.d-d-list').each(function (idx, el) {
+        $(el).click( function (e) {
+            debugger
+            
+            const current = $(this).find('p');
+
+            $(this).toggleClass('active');
+
+            $(this).find('li').click(function(e) {
+
+                console.log(this.innerText) // Выводит нужный текст. Текст из <li></li>
+
+                console.log(current.innerText) // Пишет, что undefine
+                
+                current.innerText = this.innerText; // Естественно, не работает
+            });
+
+            // function DropDown(el) {
+            //     this.dd = el;
+            //     this.initEvents();
+            // }
+
+            // DropDown.prototype = {
+            //     initEvents : function() {
+            //         var obj = this;
+        
+            //         obj.dd.on('click', function(event){
+            //             $(this).toggleClass('active');
+            //             event.stopPropagation();
+            //         });	
+            //     }
+            // }
+
+            // var dd = new DropDown( $('.') );
+
+            // $(document).click(function() {
+            //     // all dropdowns
+            //     $('.d-d-list').removeClass('active');
+            // });
 
             
 
-    //         function DropDown(el) {
-    //             this.dd = el;
-    //             this.initEvents();
-    //         }
+        });
+    });
 
-    //         DropDown.prototype = {
-    //             initEvents : function() {
-    //                 var obj = this;
-        
-    //                 obj.dd.on('click', function(event){
-    //                     $(this).toggleClass('active');
-    //                     event.stopPropagation();
-    //                 });	
-    //             }
-    //         }
 
-    //         var dd = new DropDown( $('.') );
+    // Хрень ниже работает, но только на первый элемент, а потому и нужен этот цирк с конями
+    
+    // 1
+    // function DropDown(el) {
+    //     this.dd = el;
+    //     this.initEvents();
+    // }
 
-    //         $(document).click(function() {
-    //             // all dropdowns
-    //             $('.d-d-list').removeClass('active');
+    // // 2
+    // DropDown.prototype = {
+    //     initEvents : function() {
+    //         var obj = this;
+    //         let curr = this.dd.$(".current");
+    //         let option = this.dd.$("li");
+
+    //         obj.dd.on('click', function(event){
+    //             $(this).toggleClass('active');
+    //             event.stopPropagation();
     //         });
 
-            
+    //         obj.dd.iption.on('click', function(event){
+    //             $(this).toggleClass('hidden');
+    //             event.stopPropagation();
+    //         });
+    //     }
+    // }
 
+    // // 3
+    
+    // $(function() {
+
+    //     var dd = new DropDown( $('.d-d-list') );
+
+    //     $(document).click(function() {
+    //         // all dropdowns
+    //         $('.d-d-list').removeClass('active');
     //     });
+
     // });
-
-
-    // 1
-    function DropDown(el) {
-        this.dd = el;
-        this.initEvents();
-    }
-
-    // 2
-    DropDown.prototype = {
-        initEvents : function() {
-            var obj = this;
-            let curr = this.dd.$(".current");
-            let option = this.dd.$("li");
-
-            obj.dd.on('click', function(event){
-                $(this).toggleClass('active');
-                event.stopPropagation();
-            });
-            
-            obj.dd.iption.on('click', function(event){
-                $(this).toggleClass('hidden');
-                event.stopPropagation();
-            });
-        }
-    }
-
-    // 3
-    $(function() {
-
-        var dd = new DropDown( $('.d-d-list') );
-
-        $(document).click(function() {
-            // all dropdowns
-            $('.d-d-list').removeClass('active');
-        });
-
-    });
 
 
 
